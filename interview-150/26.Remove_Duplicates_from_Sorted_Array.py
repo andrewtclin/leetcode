cfbfb244b8ef9ef86d2f95b5ba646643eefff07a
TC Lin <chuntcdj@gmail.com>
2024-04-16 21:53:59 +1000

    Updating leetcode 26

---
+++ interview-150/26.Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def removeDuplicates(nums: List[int]) -> int:
+    pointer = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i-1]:
+            nums[pointer] = nums[i]
+            pointer += 1
+    return pointer
