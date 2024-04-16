cbfd28cc3cfaf1fff77198f3f5d0741253bd02b9
TC Lin <chuntcdj@gmail.com>
2024-04-16 19:15:39 +1000

    Updating leetcode 27

--- interview-150/27.Remove_Element.py
+++ interview-150/27.Remove_Element.py
@@ -2,9 +2,9 @@ from typing import List
 
 def removeElement(nums: List[int], val: int) -> int:
         pointer = 0
-        for idx, num in enumerate(nums):
-            if num != val:
-                nums[pointer] = num
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[pointer] = nums[i]
                 pointer += 1
         return pointer
 
