663dce586527c8b4f13d53c466b9bd576d50d40c
TC Lin <chuntcdj@gmail.com>
2024-05-05 09:01:40 +1000

    Update solutions

--- interview-150/27.py
+++ interview-150/27.py
@@ -3,9 +3,17 @@ from typing import List
 def removeElement(nums: List[int], val: int) -> int:
         pointer = 0
         for i in range(len(nums)):
-            if nums[i] != val:
-                nums[pointer] = nums[i]
-                pointer += 1
+                if nums[i] != val:
+                        nums[pointer] = nums[i]
+                        pointer += 1
         return pointer
 
-#[3.2.2.3]
+        # solution 2
+        # l_ptr, r_ptr = 0, 0
+
+        # while r_ptr < len(nums):
+        #     if nums[r_ptr] != val:
+        #         nums[l_ptr] = nums[r_ptr]
+        #         l_ptr += 1
+        #     r_ptr += 1
+        # return l_ptr
--- interview-150/88.py
+++ interview-150/88.py
@@ -4,25 +4,50 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     """
     Do not return anything, modify nums1 in-place instead.
     """
+
+    # solution 1
     if n == 0:
         return nums1
-    
-    nums2_pointer = n - 1
-    nums1_pointer = m - 1
-    merged_array_pointer = len(nums1) - 1
 
-    while nums2_pointer >= 0:
-        if nums2[nums2_pointer] >= nums1[nums1_pointer]:
-            nums1[merged_array_pointer] = nums2[nums2_pointer]
-            nums2_pointer -= 1
+    nums1_ptr = m - 1
+    nums2_ptr = n - 1
+    merged_ptr = len(nums1) - 1
+
+    while nums2_ptr >= 0:
+        if nums2[nums2_ptr] > nums1[nums1_ptr]:
+            nums1[merged_ptr] = nums2[nums2_ptr]
+            nums2_ptr -= 1
         else:
-            nums1[merged_array_pointer] = nums1[nums1_pointer]
-            nums1_pointer -= 1
-            if nums1_pointer < 0:
-                i = 0
-                for element in nums2[:nums2_pointer+1]:
-                    nums1[i] = element
-                    i += 1
+            nums1[merged_ptr] = nums1[nums1_ptr]
+            nums1_ptr -= 1
+
+            if nums1_ptr < 0:
+                for i in range(nums2_ptr + 1):
+                    nums1[i] = nums2[i]
                 break
-        merged_array_pointer -= 1
+        merged_ptr -= 1
     return nums1
+
+    # solution 2
+    # if n == 0:
+    #     return nums1
+    
+    # nums2_pointer = n - 1
+    # nums1_pointer = m - 1
+    # merged_array_pointer = len(nums1) - 1
+
+    # while nums2_pointer >= 0:
+    #     if nums2[nums2_pointer] >= nums1[nums1_pointer]:
+    #         nums1[merged_array_pointer] = nums2[nums2_pointer]
+    #         nums2_pointer -= 1
+    #     else:
+    #         nums1[merged_array_pointer] = nums1[nums1_pointer]
+    #         nums1_pointer -= 1
+    #         if nums1_pointer < 0:
+    #             i = 0
+    #             for element in nums2[:nums2_pointer+1]:
+    #                 nums1[i] = element
+    #                 i += 1
+    #             break
+    #     merged_array_pointer -= 1
+    # return nums1
